b3e964d9101dae1ec02ce078aa4ae3c29b06816b
Ilnaz Sharifullin <89172610217ilnaz2000@mail.ru>
2021-05-26 20:13:54 +0300

    added registration page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import '../components/app.css';
 import {BrowserRouter, Route, Switch} from "react-router-dom"
 import Navbar from './navbar/Navbar'
-import Login from './auth/Login';
+import Login from './auth/Login'
+import Registration from './registration/Registration'
 import Goal from './goal/Goal'
 import Profile from './profile/Profile'
 import Profile_edit from './profile_edit/Profile_edit'
@@ -15,6 +16,7 @@ function App() {
 		    <div className="content">
     <Switch>
       <Route path='/login' component={Login}></Route>
+      <Route path='/registration' component={Registration}></Route>
       <Route path='/goal' component={Goal}></Route>
       <Route path='/profile' component={Profile}></Route>
       <Route path='/profile_edit' component={Profile_edit}></Route>
--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,21 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import './login.css'
+import Input from '../utils/Input'
+import axios from 'axios'
 
 export default function Login() {
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function getToken() {
+
+
+    await fetch('https://progress-up.herokuapp.com/v1/tokens', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "authorization": {
+              "email": email,
+              "password": password
+            }
+        })
+}).then(res => {
+        console.log(res);
+        console.log(res.json());
+      })
+}	   
+
+
 	return (
 		<div className="login">
 			<div className="login__title">Вход</div>
 				<form action="" className="login__form">
 					<div className="login__form_email">
 						<div className="login__form_email_text">Email</div>
-						<input type="text" placeholder="введите email"/>
+						<Input value={email} setValue={setEmail} type="text" placeholder="введите email"/>
 					</div>
 					<div className="login__form_password">
 						<div className="login__form_password_text">Password</div>
-						<input type="password" placeholder="введите пароль"/>
+						<Input value={password} setValue={setPassword} type="password" placeholder="введите пароль"/>
 					</div>
 					</form>
-				<button className="login__button" type="submit">Войти</button>
+				<button className="login__button" type="submit" onClick={getToken}>Войти</button>
 		</div>
 	)
 }
